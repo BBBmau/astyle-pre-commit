5cbe64345e6ff5c58ec20f1b7e09e79ae95f81a3
BBBmau <alvarez.mauriciotm@gmail.com>
2024-05-15 17:18:38 -0700

    use .orig

--- astyle.sh
+++ astyle.sh
@@ -16,13 +16,13 @@ git diff --cached --name-status --diff-filter=ACMR |
 	# RETURN of the parent hasn't changed properly.
 	while read STATUS FILE; do
 	if [[ "$FILE" =~ ^.+(c|cpp|h)$ ]]; then
-		$ASTYLE $OPTIONS < $FILE > $FILE.beautified
-		md5sum -b $FILE | { read stdin; echo $stdin.beautified; } | md5sum -c --status -
+		$ASTYLE $OPTIONS < $FILE > $FILE.orig
+		md5sum -b $FILE | { read stdin; echo $stdin.orig; } | md5sum -c --status -
 		if [ $? -ne 0 ]; then
 			echo "[!] $FILE does not respect the agreed coding standards." >&2
 			RETURN=1
 		fi
-		rm $FILE.beautified
+		rm $FILE.orig
 	fi
 	done
 
